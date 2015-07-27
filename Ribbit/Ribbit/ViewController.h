bfb516127ed71913d5e2c14bae08917d646772be
Dean Laurea <deanlaurea@gmail.com>
2015-07-26 19:24:41 -0500

    Create new project Ribbit

---
+++ Ribbit/Ribbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ribbit
+//
+//  Created by Dean Laurea on 7/26/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

c2d73eb8413b6f6c57698ec028fd0a961f2825b3
Dean Laurea <deanlaurea@icloud.com>
2015-07-26 21:53:49 -0500

    Add login and sign up screens

---
+++ Ribbit/Ribbit/InboxTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxTableViewController.h
+//  Ribbit
+//
+//  Created by Dean Laurea on 7/26/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxTableViewController : UITableViewController
+
+@end

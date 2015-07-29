f38a22b511c3e6c498ca3597e8712e28847f2c9e
Dean Laurea <deanlaurea@icloud.com>
2015-07-28 22:20:51 -0500

    Create login and signup view controllers

---
+++ Ribbit/Ribbit/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Ribbit
+//
+//  Created by Dean Laurea on 7/28/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end

dbd26e23dbadc242a080efd8639b558847c7dd19
Dean Laurea <deanlaurea@icloud.com>
2015-07-28 22:43:03 -0500

    Add properties and implementation for signup view controller

--- Ribbit/Ribbit/SignupViewController.m
+++ Ribbit/Ribbit/SignupViewController.m
@@ -34,4 +34,16 @@
 }
 */
 
+- (IBAction)signup:(id)sender {
+    NSString *username = [self.usernameField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *email = [self.emailField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    if ([username length] == 0 || [password length] == 0 || [email length] == 0) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Oops!" message:@"Make sure you enter a username, password, and email address!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        
+        [alertView show];
+    }
+    
+}
 @end

358d88373d3e6073f7f299aa206b5f3a3d1260c1
Dean Laurea <deanlaurea@icloud.com>
2015-07-31 18:11:47 -0500

    Add EditFriendsTableViewController and add constraints to username and password text fields

---
+++ Ribbit/Ribbit/EditFriendsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  EditFriendsTableViewController.h
+//  Ribbit
+//
+//  Created by Dean Laurea on 7/31/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditFriendsTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *allUsers;
+
+
+@end
---
+++ Ribbit/Ribbit/EditFriendsTableViewController.m
@@ -0,0 +1,105 @@
+//
+//  EditFriendsTableViewController.m
+//  Ribbit
+//
+//  Created by Dean Laurea on 7/31/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import "EditFriendsTableViewController.h"
+#import <Parse/Parse.h>
+
+@interface EditFriendsTableViewController ()
+
+@end
+
+@implementation EditFriendsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    PFQuery *query = [PFUser query];
+    [query orderByAscending:@"username"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+    
+        if (error) {
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+      
+        } else {
+            self.allUsers = objects;
+            [self.tableView reloadData];
+        }
+    }];
+  
+}
+
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    
+    return self.allUsers.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    cell.textLabel.text = user.username;
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

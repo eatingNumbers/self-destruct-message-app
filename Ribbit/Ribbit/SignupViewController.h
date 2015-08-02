6f575ac6c978532d55b9e1fd5467fbe426ab7b85
Dean Laurea <deanlaurea@icloud.com>
2015-08-02 02:30:57 -0500

    Add textfield properties to SignupViewController.h

--- Ribbit/Ribbit/SignupViewController.h
+++ Ribbit/Ribbit/SignupViewController.h
@@ -9,9 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface SignupViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *firstNameField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameField;
+@property (weak, nonatomic) IBOutlet UITextField *hometownField;
+@property (weak, nonatomic) IBOutlet UITextField *websiteField;
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
 @property (weak, nonatomic) IBOutlet UITextField *emailField;
+
 
 - (IBAction)signup:(id)sender;
 
